5143213256d5a69ef8d40483758cba86a5144840
Alex <kosianlucky@mail.ru>
2021-11-15 20:13:56 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+  
+    $nickname = "Александр";
+    $recommend = "Этот сайт одновременно и учебный и рабочий по PHP";
+    $balance = 49;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мой учебный сайт по PHP ПРОДОЛЖЕНИЕ</title>
+    <!-- Добавляем шрифты с гугл -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&family=Cormorant:wght@300&family=Montserrat+Alternates:ital,wght@0,100;1,100&family=Philosopher&family=Zen+Antique&display=swap" rel="stylesheet">
+    <!-- Добавили файл нормалайз -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- подключили наш файл CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css"></link>
+</head>
+<body>
+<h1>Мой учебный сайт по PHP - ПРОДОЛЖЕНИЕ</h1>
+<h5 class="subtitle">Часть 2</h5>
+
+    
+</body>
+</html>
